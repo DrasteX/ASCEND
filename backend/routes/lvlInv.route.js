3f0e0b638a1f9568501f1325a8066603231e2a92
DrasteX <tushardesktop1@gmail.com>
2025-05-12 21:17:55 +0530

    more fixes

--- backend/routes/lvlInv.route.js
+++ backend/routes/lvlInv.route.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const lvlInv = require('../models/lvlInv.model.js');
 
-router.get('/', async(req,res)=>{
+router.get('/', async (req,res)=>{
     res.send('LEVELING AND INVENTORY SYSTEM API')
 })
 
-router.post('/add', async(req,res)=>{
+router.post('/add', async (req,res)=>{
     try {
         const li = lvlInv.create(req.body);
         res.status(200).json(li)
@@ -15,16 +15,16 @@ router.post('/add', async(req,res)=>{
         res.status(500).json({message:err.message})
     }
 })
-router.get('/find', async(req,res)=>{
+router.get('/find', async (req,res)=>{
     try {
-        const li = lvlInv.findById(res.query.id);
+        const li = lvlInv.findById(req.query.id);
         res.status(200).json(li)
     }
     catch(err){
         res.status(500).json({message:err.message})
     }
 })
-router.get('/listall', async(req,res)=>{
+router.get('/listall', async (req,res)=>{
     try {
         const li = lvlInv.find()
         res.status(200).json(li)
@@ -34,7 +34,7 @@ router.get('/listall', async(req,res)=>{
     }
 })
 
-router.put('/update', async(req,res)=>{
+router.put('/update', async (req,res)=>{
     try {
         const li = lvlInv.findByIdAndUpdate(req.query.id, req.body, {new:true});
         res.status(200).json(li)
@@ -44,7 +44,7 @@ router.put('/update', async(req,res)=>{
     }
 })
 
-router.delete('/delete', async(req,res)=>{
+router.delete('/delete', async (req,res)=>{
     try {
         const li = lvlInv.findByIdAndDelete(req.query.id);
         res.status(200).json(li)
